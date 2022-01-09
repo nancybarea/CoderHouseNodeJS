3328f5fc75722419633c6e704ae005ea43748a1a
nancy <nancybarea@gmail.com>
2022-01-09 20:33:05 -0300

    clase contador y variable static

---
+++ clase01_ejercicio02/main.js
@@ -0,0 +1,53 @@
+/* Ejercicio
+Construir una herramienta que permita que diferentes personas puedan llevar cuentas individuales 
+sobre algo que deseen contabilizar, al mismo tiempo que nos brinde una contabilidad general 
+del total contado. Para ello:
+- Definir la clase Contador.
+- Cada instancia de contador debe ser identificada con el nombre de la persona responsable de ese conteo.
+- Cada instancia inicia su cuenta individual en cero.
+- La clase en sí misma posee un valor estático con el que lleva la cuenta de todo lo contado 
+por sus instancias, el cual también inicia en cero.
+*/
+
+class Contador{
+
+    constructor (nombre) {
+        this.nombre = nombre;
+        this.contador = 0;
+    }
+
+    static contadorTotal = 0;
+
+    obtenerResponsable () {
+        console.log(`El nombre es ${this.nombre}`);
+    }
+
+    obtenerCuentaIndividual(){
+        console.log(`La cuenta individual es ${this.contador}`);
+    }
+
+    obtenerCuentaGlobal(){
+        console.log(`La cantidad total es ${Contador.contadorTotal}`);
+    }
+
+    contar(){
+        this.contador ++;
+        Contador.contadorTotal ++;
+    }
+}
+
+const contadorTelecom = new Contador("Nancy");
+const contadorTelefonica = new Contador("Raul");
+console.log("Telecom: ");
+contadorTelecom.obtenerResponsable;
+contadorTelecom.obtenerCuentaIndividual;
+console.log("Telefonica");
+contadorTelefonica.obtenerResponsable;
+contadorTelefonica.obtenerCuentaIndividual;
+console.log("incremento en 1 contador Telecom");
+contadorTelecom.contar();
+console.log("incremento en 2 contador telefonica");
+contadorTelefonica.contar();
+contadorTelefonica.contar();
+console.log("el incremento total es: ");
+console.log(Contador.contadorTotal);
