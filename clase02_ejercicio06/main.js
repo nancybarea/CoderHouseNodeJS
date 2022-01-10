68b02512098be6ac96df37badfc4cd3fa559d46b
nancy <nancybarea@gmail.com>
2022-01-09 22:06:29 -0300

    Try/Catch, TrimEnd y ??

---
+++ clase02_ejercicio06/main.js
@@ -0,0 +1,11 @@
+/*
+Definir un método 'mostrar' dentro de una clase Mensaje que reciba dos parámetros: el mensaje a representar por consola y el retardo de tiempo en segundos luego del cual se mostrará el mensaje.
+El retardo podrá ser cero y en el caso de no definirlo se usará el valor de un segundo.
+Para el caso de no definir ni el retardo ni el mensaje mostrar la frase: 'defina mensaje'.
+Permitir que ingresen mensajes como un 0 o false y que puedan ser representados.
+Si el mensaje posee espacios al inicio dejarlos tal cual, sólo remover los espacios al final.
+Verificar que la operación anterior sea válida para todos los casos, caso contrario no realizarla.
+Utilizar funciones de ES10, ES11: Try/Catch, TrimEnd y ??
+*/
+
+// NO LO HICIMOS EN CLASE, HACER CUANDO PUEDA Y PASAR AL TUTOR.
