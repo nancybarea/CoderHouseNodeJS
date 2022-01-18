2c74fc7dc12b63bbcf21a1c6309cfe28c5e018e5
nancy <nancybarea@gmail.com>
2022-01-18 14:05:04 -0300

    ejercicio 2 socket io de clase con handlebars

---
+++ clase10_ejercicio03/public/index.js
@@ -0,0 +1,24 @@
+const socket = io(); // Ya podemos empezar a usar los sockets desde el cliente
+
+// Cliente
+socket.on('Mensaje', data => {
+    console.log(data);
+    render(data);
+})
+
+function render (data){
+    let html = data.map(function(elem, index){
+        return(`<div>${elem.socketid} : ${elem.mensaje}</diV>`)
+    }).join(" ");    
+
+    document.getElementById("listadoMensajes").innerHTML = html;
+}
+
+function addMessage(e){
+    let nuevoMensaje = {
+        socketid: socket.io.engine.id,
+        mensaje: document.getElementById("mensaje").value
+    };
+    socket.emit("nuevo-mensaje", nuevoMensaje);
+    return false;
+}
---
+++ clase10_ejercicio03/server.js
@@ -0,0 +1,53 @@
+// De donde saque info para hacer este ejercicio.
+//https://www.youtube.com/watch?v=ppiAvvkvAz0
+
+const express = require('express')
+const { Server: HttpServer } = require('http')
+const { Server: IOServer } = require('socket.io')
+
+const app = express()
+const httpServer = new HttpServer(app)
+const io = new IOServer(httpServer)
+
+const { engine } = require("express-handlebars");
+
+// Indicamos que queremos cargar los archivos estáticos que se encuentran en dicha carpeta
+app.use(express.static('./public'))
+
+// engine(extesion, callback)
+app.engine(
+    "hbs", // nombre del motor / plantilla  
+    engine({ //engine viene del nombre como lo importe  const { engine } = require("express-handlebars");
+      extname: ".hbs", // extension de los archivos, si no ponemos por defecto va ser .handlebars
+      defaultLayout: "layout.hbs", //plantilla principal
+      layoutsDir: __dirname + "/views/layouts", //ruta de la plantilla principal
+      partialsDir: __dirname + "/views/partials", // ruta a las plantillas parciales
+    })
+  );
+app.set("views", "./views");  //ubicacion de los archivos de plantilla
+app.set("view engine", "hbs"); //motor de plantilla q vamos a utilizar "hbs"
+
+
+let mensajes = [{socketid: 1, mensaje: "Inicio de Mensajes"}]
+
+// Esta ruta carga nuestro archivo index.html en la raíz de la misma
+app.get('/', (req, res) => {
+    res.render("envioMensajes", mensajes);
+})
+
+//inicializamos el canal de websockets
+io.on('connection', socket => {
+    // "connection" se ejecuta la primera vez que se abre una nueva conexión
+    console.log('¡Nuevo Cliente conectado!')
+    //envio datos al cliente (desde servidor)
+    socket.emit('Mensaje', mensajes ) // (evento, msg)
+
+    socket.on('nuevo-mensaje', data => {
+        mensajes.push(data)  //agrego al array el nuevo mensaje
+        io.sockets.emit('Mensaje', mensajes); // para avisar a todos que llego un nuevo mensaje
+    })
+   
+})
+
+// El servidor funcionando en el puerto 8080
+httpServer.listen(8080, () => console.log('SERVER ON'))
