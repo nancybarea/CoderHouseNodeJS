5c7206283488362ad0620918144e18871ed4b323
nancy <nancybarea@gmail.com>
2022-02-03 19:55:24 -0300

    websocks chat como dice ppt (onsubmit)

---
+++ clase11_ejercicio01/public/js/main.js
@@ -0,0 +1,23 @@
+const socket = io.connect();
+
+function addMessage(e) {
+    const mensaje = {
+        author: document.getElementById('username').value,
+        text: document.getElementById('texto').value
+    };
+    socket.emit('new-message', mensaje);
+    return false;
+}
+
+
+function render(data) {
+    const html = data.map((elem, index) => {
+        return(`<div>
+            <strong>${elem.author}</strong>:
+            <em>${elem.text}</em> </div>`)
+    }).join(" ");
+    document.getElementById('messages').innerHTML = html;
+}
+
+
+socket.on('messages', function(data) { render(data); });
---
+++ clase11_ejercicio01/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { Server: HttpServer } = require("http");
+const { Server: IOServer } = require("socket.io");
+
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
+
+app.use(express.static("public"));
+
+//mensaje a enviar cuando se conecta un cliente
+const messages = [
+    { author: "Juan", text: "¡Hola! ¿Que tal?" },
+    { author: "Pedro", text: "¡Muy bien! ¿Y vos?" },
+    { author: "Ana", text: "¡Genial!" }
+ ];
+
+//servidor atento a que se realice una nueva conexion
+io.on('connection',socket => {
+    console.log('Un cliente se ha conectado');
+    socket.emit('messages', messages);
+
+    socket.on('new-message',data => {
+        messages.push(data);
+        io.sockets.emit('messages', messages);
+    });
+ });
+
+
+//servidor escuchando en puerto 8080
+httpServer.listen(8080, function() {
+    console.log('Servidor corriendo en http://localhost:8080');
+})
